d40c271a832dba1d6f22cdd3df07df0065b4c213
D-Bhatta <dbhatta1232@gmail.com>
2020-10-05 14:22:53 -0400

    Create Category, Post, and Comment models

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+# Category model
+
+
+class Category(models.Model):
+    """
+    Model for categories. Each instance is a category.
+    """
+
+    name = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    """
+    Model for all posts. Each instance is a post with title, body, and dates
+    of creation and modification. Also has a many to many relation to
+    Categories.
+    """
+
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField("Category", related_name="posts")
+
+
+class Comments(models.Model):
+    """
+    Model for all comments. Each instance is a comment with author, body,
+    created_on, and the post it is related to is a ForeignKey.
+    """
+
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
